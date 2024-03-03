752a1d8d7864339753946f6a4b13478cc6f244cd
asekus <stanislawork@gmail.com>
2024-03-03 12:57:00 +0100

    lambda init

--- netlify/functions/go.mod
+++ netlify/functions/go.mod
@@ -3,6 +3,7 @@ module github.com/asekus/stripe/test1
 go 1.21.6
 
 require (
+	github.com/aws/aws-lambda-go v1.46.0
 	github.com/gorilla/mux v1.8.1
 	github.com/stripe/stripe-go v70.15.0+incompatible
 	github.com/stripe/stripe-go/v76 v76.19.0 // indirect
--- netlify/functions/main.go
+++ netlify/functions/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/stripe/stripe-go/v76"
 	"github.com/stripe/stripe-go/v76/customer"
 	"github.com/stripe/stripe-go/v76/ephemeralkey"
 	"github.com/stripe/stripe-go/v76/paymentintent"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	if request.HTTPMethod != "POST" {
+		return &events.APIGatewayProxyResponse{Body: http.StatusText(http.StatusMethodNotAllowed), StatusCode: http.StatusMethodNotAllowed}, nil
 	}
 
 	// Use an existing Customer ID if this is a returning customer.
@@ -42,7 +42,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	pi, _ := paymentintent.New(piparams)
 
-	writeJSON(w, struct {
+	responseBody, _ := json.Marshal(struct {
 		PaymentIntent  string `json:"paymentIntent"`
 		EphemeralKey   string `json:"ephemeralKey"`
 		Customer       string `json:"customer"`
@@ -53,65 +53,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		Customer:       c.ID,
 		PublishableKey: "pk_test_51OpcPfHVyvJVrH9E6fZD7MehGxcmrvAGaX1y1gWrQrUgXlki94f9MlnxWUoheeE6my1fITxokmAojqHtjQisolxp00VpgM8Zb3",
 	})
+
+	return &events.APIGatewayProxyResponse{Body: string(responseBody), StatusCode: http.StatusOK}, nil
 }
 
 func main() {
 	// This is your test secret API key.
 	stripe.Key = "sk_test_51OpcPfHVyvJVrH9E5AEuUFa7ynSwVCL2KhaI6xtOSTfmSjBFRUnn2O6tDlYOkyjHsfawONwvb6LPE4JCbnw2g90k0011fynIOm"
 
-	r := mux.NewRouter()
-	r.HandleFunc("/create-payment-intent", handler)
-	//http.HandleFunc("/create-payment-intent", handlePaymentSheet)
-
-	addr := ":8081"
-	log.Printf("Listening on %s ...", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
-}
-
-func handlePaymentSheet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Use an existing Customer ID if this is a returning customer.
-	cparams := &stripe.CustomerParams{}
-	c, _ := customer.New(cparams)
-
-	ekparams := &stripe.EphemeralKeyParams{
-		Customer:      stripe.String(c.ID),
-		StripeVersion: stripe.String("2023-10-16"),
-	}
-	ek, _ := ephemeralkey.New(ekparams)
-
-	piparams := &stripe.PaymentIntentParams{
-		Amount:   stripe.Int64(1099),
-		Currency: stripe.String(string(stripe.CurrencyEUR)),
-		Customer: stripe.String(c.ID),
-		PaymentMethodTypes: []*string{
-			stripe.String("bancontact"),
-			stripe.String("card"),
-			stripe.String("ideal"),
-			stripe.String("klarna"),
-			stripe.String("sepa_debit"),
-		},
-	}
-	pi, _ := paymentintent.New(piparams)
-
-	writeJSON(w, struct {
-		PaymentIntent  string `json:"paymentIntent"`
-		EphemeralKey   string `json:"ephemeralKey"`
-		Customer       string `json:"customer"`
-		PublishableKey string `json:"publishableKey"`
-	}{
-		PaymentIntent:  pi.ClientSecret,
-		EphemeralKey:   ek.Secret,
-		Customer:       c.ID,
-		PublishableKey: "pk_test_51OpcPfHVyvJVrH9E6fZD7MehGxcmrvAGaX1y1gWrQrUgXlki94f9MlnxWUoheeE6my1fITxokmAojqHtjQisolxp00VpgM8Zb3",
-	})
-}
-
-func writeJSON(w http.ResponseWriter, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	lambda.Start(handler)
 }
